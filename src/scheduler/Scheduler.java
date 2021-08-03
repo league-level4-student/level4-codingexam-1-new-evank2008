1cfade30300d5eef0f107f566153fd1eaee4c992
codybsauer <47398539+codybsauer@users.noreply.github.com>
2021-08-03 14:00:38 -0700

    first commit

---
+++ src/scheduler/Scheduler.java
@@ -0,0 +1,26 @@
+package scheduler;
+/*
+ * Objective: Create a weekly scheduling application.
+ * 
+ * You may create any additional enums, classes, methods or variables needed
+ * to accomplish the requirements below:
+ * 
+ * - You should use an array filled with enums for the days of the week and each
+ *   enum should contain a LinkedList of events that include a time and what is 
+ *   happening at the event.
+ * 
+ * - The user should be able to to interact with your application through the
+ *   console and have the option to add events, view events or remove events by
+ *   day.
+ *   
+ * - Each day's events should be sorted by chronological order(Hint: use the 
+ *   properties of LinkedLists to place the event in the right spot rather than 
+ *   try to sort it afterwards).
+ *  
+ * - If the user tries to add an event on the same day and time as another event
+ *   throw a SchedulingConflictException(created by you) that tells the user
+ *   they tried to double book a time slot.
+ */
+public class Scheduler {
+   
+}
